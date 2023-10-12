aa7a0577c8d77ac7ce7501e63aca341b68a9a1cc
seok-pyo <hongseokpyou@gmail.com>
2023-10-12 22:23:13 +0900

    시간 측정 함수

---
+++ script.js
@@ -0,0 +1,26 @@
+// const btn1 = document.querySelector(".button1");
+// const btn2 = document.querySelector(".button2");
+// let t1;
+
+// function d1() {
+//   t1 = Date.now();
+// }
+
+// function d2(t1, t2) {
+//   let sec = parseInt((t2 - t1) / 1000);
+//   let hour = parseInt(sec / 3600);
+//   let min = parseInt((sec % 3600) / 60);
+//   let fsec = (sec % 3600) % 60;
+//   return `${hour}시간, ${min}분, ${fsec}초`;
+// }
+
+// btn1.addEventListener("click", () => {
+//   localStorage.setItem("t1", Date.now());
+//   const t1 = localStorage.getItem("t1");
+//   console.log(t1);
+// });
+
+// btn2.addEventListener("click", () => {
+//   let t2 = Date.now();
+//   console.log(d2(t1, t2));
+// });
